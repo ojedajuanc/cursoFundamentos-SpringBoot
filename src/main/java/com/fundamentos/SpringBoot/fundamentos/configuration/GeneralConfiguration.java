cddee47ffcbf9ac64310635798f3fec9b9840bff
charly5683 <ojeda.juancarlos02@gmail.com>
2022-01-21 21:16:11 -0300

    Configuracion de datasource

--- src/main/java/com/fundamentos/SpringBoot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/SpringBoot/fundamentos/configuration/GeneralConfiguration.java
@@ -1,7 +1,10 @@
 package com.fundamentos.SpringBoot.fundamentos.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +14,7 @@ import com.fundamentos.SpringBoot.fundamentos.model.User;
 
 @Configuration
 @EnableConfigurationProperties(User.class)
-public class ConfiguracionValores {
+public class GeneralConfiguration {
 	@Value("${value.nombre}")
 	private String nombre;
 	
@@ -21,6 +24,16 @@ public class ConfiguracionValores {
 	@Bean
 	public NombreCompleto funcion() {
 		return new NombreCompletoImpl(nombre, apellido);
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.h2.Driver");
+		dataSourceBuilder.url("jdbc:h2:mem:testdb");
+		dataSourceBuilder.username("user");
+		dataSourceBuilder.password("user");
+		return dataSourceBuilder.build();
 	}
 
 }
