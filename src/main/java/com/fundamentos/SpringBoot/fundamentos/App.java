6112008440995a2793ca264f7fe0ded4ba02e56d
charly5683 <ojeda.juancarlos02@gmail.com>
2022-01-27 16:54:53 -0300

    add JPQL queries

--- src/main/java/com/fundamentos/SpringBoot/fundamentos/App.java
+++ src/main/java/com/fundamentos/SpringBoot/fundamentos/App.java
@@ -1,61 +1,86 @@
 package com.fundamentos.SpringBoot.fundamentos;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
-import com.fundamentos.SpringBoot.fundamentos.bean.MyBean1;
-import com.fundamentos.SpringBoot.fundamentos.bean.NombreCompleto;
-import com.fundamentos.SpringBoot.fundamentos.component.ComponentDependency;
-import com.fundamentos.SpringBoot.fundamentos.model.User;
-import com.fundamentos.SpringBoot.fundamentos.services.BeanWithDependencies;
+import com.fundamentos.SpringBoot.fundamentos.persistence.User;
+import com.fundamentos.SpringBoot.fundamentos.repository.UserRepository;
+
+import sun.util.logging.resources.logging;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
 	
+	private UserRepository userRepository;
+	
 	private final Log LOGGER = LogFactory.getLog(this.getClass());
+//	private ComponentDependency componentDependency;
+//	private MyBean1 myBean;
+//	private BeanWithDependencies beanWithDependencie;
+//	private NombreCompleto nombreCompleto;
+//	private User u1;
 
-	private ComponentDependency componentDependency;
-	private MyBean1 myBean;
-	private BeanWithDependencies beanWithDependencie;
-	private NombreCompleto nombreCompleto;
-	private User u1;
 
 	/**
 	 * Se utiliza el annotation Qualifier para que SpringBoot sepa a qué componente
 	 * llamar cuando dos o más tienen misma implementación
 	 */
-	public App(@Qualifier("component2Impl") ComponentDependency componentDependency,
-			MyBean1 myBean, BeanWithDependencies beanWithDependencie, NombreCompleto nombreCompleto,
-			User u1) {
-		this.componentDependency = componentDependency;
-		this.myBean = myBean;
-		this.beanWithDependencie = beanWithDependencie;
-		this.nombreCompleto = nombreCompleto;
-		this.u1 = u1;
-	}
+//	public App(@Qualifier("component2Impl") ComponentDependency componentDependency,
+//			MyBean1 myBean, BeanWithDependencies beanWithDependencie, NombreCompleto nombreCompleto,
+//			User u1) {
+//		this.componentDependency = componentDependency;
+//		this.myBean = myBean;
+//		this.beanWithDependencie = beanWithDependencie;
+//		this.nombreCompleto = nombreCompleto;
+//		this.u1 = u1;
+//	}
 
+	public App(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		componentDependency.saludar();
-		System.out.println(beanWithDependencie.operationWithDependencies());
-		System.out.println("Nombre: " + nombreCompleto.function());
-		System.out.println("Mail: " + u1.getEmail() + " Edad: " + u1.getAge());
-		LOGGER.error("Error de prueba");
-		try {
-			int value = u1.getAge() / 0;
-			System.out.println(value);
-		} catch (Exception e) {
-			LOGGER.error("No se ingresó un número");
-		}
+		saveUsersInDataBase();
+		getInfoFromJPQL();
+	}
+	
+	public void saveUsersInDataBase() {
+		User u1 = new User("Juan", "test1@mail.com", LocalDate.of(1980, 03, 15));
+		User u2 = new User("Mariano", "test2@mail.com", LocalDate.of(1955,03, 15));
+		User u3 = new User("Sandra", "test3@mail.com", LocalDate.of(1999, 03, 15));
+		User u4 = new User("Monica", "test4@mail.com", LocalDate.of(2003, 03, 15));
+		User u5 = new User("Emilio", "test5@mail.com", LocalDate.of(1995, 03, 15));
 		
+		List<User> usersList = Arrays.asList(u1, u2, u3, u4, u5);
+//		userRepository.saveAll(usersList);
+		usersList.forEach(userRepository::save);
+	}
+	
+	private void getInfoFromJPQL() {
+		LOGGER.info("El usuario encontrado es: " + userRepository.findByUserEmail("test2@mail.com"));
+		LOGGER.info("Lista de usuarios con mail test: ");
+		userRepository.findAndSort("test", Sort.by("id").descending())
+					.stream()
+					.forEach((user)-> LOGGER.info("Usuario: " + user.getName() + ", ID: " + user.getId()));
+	}
+	
+	private void getInfoFromJPQLWithException() {
+		LOGGER.info("El usuario encontrado es: " + 
+					userRepository.findByUserEmail("test7@mail.com")
+					.orElseThrow(()->new RuntimeException("No se encontró el usuario")));
 	}
 
 }
